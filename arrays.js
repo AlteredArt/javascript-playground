09b6864fb1a4ebb7b65c3b8148742557c9d7f01e
Altered Art <cherokeedreemer@gmail.com>
2020-06-20 01:48:51 -0600

    using arrays

--- arrays.js
+++ arrays.js
@@ -1,24 +1,31 @@
-console.log("starting of arrays");
-var characters = ['Luke', 'Leia', 'Han', 'Chewie'];
+// console.log("starting of arrays");
+// var characters = ['Luke', 'Leia', 'Han', 'Chewie'];
+//
+// console.log(characters);
+// console.log(characters.length);
+// console.log(characters[characters.length-1]);
+//
+// characters.push('Anakin');
+// console.log("push", characters);
+//
+// characters.pop();
+// console.log("pop", characters);
+//
+// characters.unshift("C3PO");
+// console.log("unshift", characters);
+//
+// var rem = characters.shift();
+// console.log(rem);
+// characters.shift("R2D2");
+// console.log("shift", characters);
+//
 
-console.log(characters);
-console.log(characters.length);
-console.log(characters[characters.length-1]);
 
-characters.push('Anakin');
-console.log("push", characters);
-
-characters.pop();
-console.log("pop", characters);
-
-characters.unshift("C3PO");
-console.log("unshift", characters);
-
-var rem = characters.shift();
-console.log(rem);
-characters.shift("R2D2");
-console.log("shift", characters);
-
-//this is our arrays for this session
-
-//next we will work on interation
+const pizza = 'pizza';
+//es5 .split("") takes a string and makes it an array
+const slicedPizza = pizza.split("");
+console.log(slicedPizza)
+//es6 using spread instead
+const slicedPizza2 = [...pizza];
+//Array.from() creatyes an array from a string
+const slicedPizza3 = Array.from(pizza);
