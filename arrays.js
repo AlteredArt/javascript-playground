37c811017ec7b86a6b83041cb4239141978c6844
Altered Art <cherokeedreemer@gmail.com>
2020-06-22 14:55:03 -0600

    for each interations

--- arrays.js
+++ arrays.js
@@ -1,3 +1,4 @@
+//arrays are numbered lists with indexes
 // console.log("starting of arrays");
 // var characters = ['Luke', 'Leia', 'Han', 'Chewie'];
 //
@@ -29,3 +30,32 @@
 // //Array.from() creatyes an array from a string
 // const slicedPizza3 = Array.from(pizza);
 // console.log(slicedPizza, slicedPizza2, slicedPizza3)
+
+// let twoD = [[1,2,3,4,5,6,7],
+//             [8, 10, 5 ,7, 3, 22, 6, 42],
+//             [123, 54, 12, 11, 9, 15]];
+//
+//             let rows = twoD.length;
+//             for(let i=0; i<rows; i++){
+//               let items =twoD[i].length;
+//               console.log(i, items)
+//               for(let t=0; t<items; t++){
+//                   console.log( twoD[i][t]);
+//               }
+//             }
+
+//for each loop, convert all the names to lower case besides thorin
+// let dwarves = ["Bifur", "Bofur", "Bombur", "Fifi", "kildi", "oin", "Glorin", "Thorin", "Balin", "Dwalin", "Nori", "Dori"];
+//
+// dwarves.forEach(function(item, index, array){
+// if( item === "Thorin"){
+//   item= item.toUpperCase();
+// }else{
+//   item= item.toLowerCase();
+// }
+//   console.log(index, item);
+// })
+
+//sorting items in an array
+var fruits = ["Bannana", "Orange", "Apple", "Mango"]
+console.log(fruits.sort());
--- objects.js
+++ objects.js
@@ -1,1 +1,40 @@
-var newObject = { day1: 1, day2: 2}
+//object literal objects are made of properties
+// var dog = {"name":"Woody",type:"dog"};
+// var cat = {"name":"Waffles", type: "cat"};
+// console.log(dog);
+// console.log(cat);
+//
+// var petNames = ["Woody", "Bob"];
+// console.log(petNames);
+// var pets = [dog, cat];
+// console.log(pets)
+
+// add to an array
+// pets.push({"name": "Chloe", type:"cat"})
+// console.log(pets[2])
+
+//add to an object
+// cat.age = 2;
+// dog["age"] = 12;
+// console.log(pets);
+//
+// pets[0].age = 14;
+// pets[2]["age"] = 6;
+// pets[1].hungry = true;
+// console.log(pets);
+
+//nested loops
+// let bigHero = {characters: [
+//   {name:"hiro", voice:"Ryan Potter"},
+//   {name:"Baymax", voice:"Scott Adsit"},
+//   {name:"Fred", voice:"T.J. Miller"}
+// ]};
+// console.log(bigHero)
+
+// let chars = bigHero.characters
+// let chars = bigHero['characters']
+//   for(let i = 0, len=chars.length; i<len; i++){
+//     console.log(chars[i]);
+//     console.log(chars[i].name);
+//     console.log(chars[i]["voice"]);
+//   }
